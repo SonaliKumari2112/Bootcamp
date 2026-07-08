a9220a7dc567acd6feb8fe66e6fbb03d41177c1b
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-07-09 03:14:15 +0530

    rotateArray

---
+++ arrayProgram/rotateArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n-1;i++){
+        cin>>a[i];
+    }
+    int miss=n*(n+1)/2;
+    for(int i=0;i<n-1;i++){
+    miss=miss-a[i];
+    }
+    cout<<"missing number"<<miss;
+    return 0;
+}
