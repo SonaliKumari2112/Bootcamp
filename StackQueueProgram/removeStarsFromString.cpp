891c12c946c37bc76a2d8e9795cb7080f551d51c
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-07-09 00:00:12 +0530

    remove star

---
+++ StackQueueProgram/removeStarsFromString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    string ans="";
+    stack<int >st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!='*'){
+            st.push(s[i]);
+        }else{
+            st.pop();
+        }
+    }
+    while(!st.empty()){
+     char top=st.top();
+     ans=ans+top;
+     st.pop();
+    }
+    reverse(s.begin(),s)
+    return 0;
+}
