0ab8b09447dcb025cad58b8b7ab045449e7ab358
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-07-11 00:09:11 +0530

    decodestring

--- StringProgram/decodestring.cpp
+++ StringProgram/decodestring.cpp
@@ -13,23 +13,33 @@ int main(){
             num=num*10+ch-'0';
         }else if(ch=='['){
             countstack.push(num);
+
             stringstack.push(curr);
+
             num=0;
+            
             curr="";
         }else if(ch==']'){
             int repeat = countstack.top();
+
             countstack.pop();
+
             string temp =stringstack.top();
+
             stringstack.pop();
+
             while(repeat--){
                 temp+=curr;
             }
             curr= temp;
 
         }else{
+
             curr+=ch;
+
         }
     }
+
     cout<<curr;
     return 0;
 }
